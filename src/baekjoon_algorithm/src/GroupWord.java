8b0f5374b1b4dfa6a031ea4c7bf807fc142bfd71
Seo Songhyeon <songhyeon4643@gmail.com>
2022-07-11 19:59:47 +0900

    Solve 1316

--- src/baekjoon_algorithm/src/GroupWord.java
+++ src/baekjoon_algorithm/src/GroupWord.java
@@ -13,10 +13,10 @@ public class GroupWord {
                 String word = sc.next();
                 boolean[] Alphabet = new boolean[26];
                 Arrays.fill(Alphabet, false);
-                for (int j = 1; j < word.length(); j++) {
-                    if (word.charAt(j) == word.charAt(j - 1)) {
-                        //continue;
-                    } else if (word.charAt(j) != word.charAt(j - 1) && Alphabet[word.charAt(j) - 97] == true) {
+                for (int j = 0; j < word.length(); j++) {
+                    if (j>0&&word.charAt(j) == word.charAt(j - 1)) {
+                        continue;
+                    } else if (j>0&&word.charAt(j) != word.charAt(j - 1) && Alphabet[word.charAt(j) - 97]==true) {
                         count++;
                         break;
                     } else{
