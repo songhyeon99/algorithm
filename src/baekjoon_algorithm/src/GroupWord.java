de92a6d05fc473f46c283e4fa865f0e789252d5b
Seo Songhyeon <songhyeon4643@gmail.com>
2022-07-11 14:44:30 +0900

    some problems

---
+++ src/baekjoon_algorithm/src/GroupWord.java
@@ -0,0 +1,31 @@
+package baekjoon_algorithm.src;
+
+import java.util.*;
+
+
+public class GroupWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자 입력하세요>> ");
+            int count=0;
+            int num = sc.nextInt();
+            for (int i = 0; i < num; i++) {
+                String word = sc.next();
+                boolean[] Alphabet = new boolean[26];
+                Arrays.fill(Alphabet, false);
+                for (int j = 1; j < word.length(); j++) {
+                    if (word.charAt(j) == word.charAt(j - 1)) {
+                        //continue;
+                    } else if (word.charAt(j) != word.charAt(j - 1) && Alphabet[word.charAt(j) - 97] == true) {
+                        count++;
+                        break;
+                    } else{
+                        Alphabet[word.charAt(j) - 97] = true;
+                    }
+                }}
+                System.out.println(num - count);
+
+        }
+
+
+}
