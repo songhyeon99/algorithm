bcd22cffc56278f91ed9fa0672e75e8bf4e0ebc7
Seo Songhyeon <songhyeon4643@gmail.com>
2023-11-01 19:01:46 +0900

    [프로그래머스] 기능개발

---
+++ src/programmers/Solution4.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.*;
+
+class Solution4 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int max=0 ,count=0;
+        List<Integer> list = new ArrayList<>();
+        int[] remainProgresses = new int [progresses.length];
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=0; i<progresses.length;i++){
+            remainProgresses[i]=100-progresses[i];
+            if(remainProgresses[i]%speeds[i]!=0) queue.add((remainProgresses[i]/speeds[i])+1);
+            else queue.add(remainProgresses[i]/speeds[i]);
+
+        }
+
+
+        while(!queue.isEmpty()){
+            int progress = queue.poll();
+            if(max<progress){
+                max=progress;
+                list.add(count+1);
+            }else{
+                list.set(list.size()-1,list.get(list.size()-1) + 1);
+            }
+        }
+
+        int [] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+
+    }
+}
+
