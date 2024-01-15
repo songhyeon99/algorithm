cd2ead49613a028a280c2dc52b6c05a41e3ca61a
Songhyeon Seo <songhyeon4643@gmail.com>
2024-01-15 23:01:59 +0900

    [level Lv.2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(date_of_birth,'%Y-%m-%d') as DATE_OF_BIRTH
+from member_profile
+where month(date_of_birth) = 3 and gender = "W" and tlno is not null
+order by member_id asc;
