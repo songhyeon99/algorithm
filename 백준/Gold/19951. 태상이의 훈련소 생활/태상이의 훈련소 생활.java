7a9818dc3530b0d157e022a6b824d7e4f46b2f2f
Songhyeon Seo <songhyeon4643@gmail.com>
2024-04-25 23:48:20 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 1536 ms, Memory: 65772 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] arr, sum;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+        sum = new int[N + 2];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            sum[A] += K;
+            sum[B+1] -= K;
+        }
+
+        for (int i = 0; i < N; i++) {
+            sum[i+1] = sum[i] + sum[i+1];
+            System.out.print(arr[i+1] + sum[i+1] + " ");
+        }
+    }
+}
