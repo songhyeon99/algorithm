5376ab77a7cd4836d9a26edafcf374979b695ee8
Songhyeon Seo <songhyeon4643@gmail.com>
2024-06-01 10:41:20 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 864 ms, Memory: 116972 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,31 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		String str = br.readLine();
+		int Q = Integer.parseInt(br.readLine());
+		int[][] alpha = new int[str.length() + 1][26];
 
-		String str = sc.nextLine();
-		int Q = sc.nextInt();
-		int[] sum = new int[str.length() + 1];
+		for (int i = 1; i <= str.length(); i++) {
+			int searchChar = str.charAt(i - 1) - 'a';
 
-		for (int i = 0; i < Q; i++) {
-			String c = sc.next();
-			int start = sc.nextInt();
-			int end = sc.nextInt();
-			int count = 0;
-			for (int j = 0; j < str.length(); j++) {
-				if (str.charAt(j) == c.charAt(0)) {
-					count++;
-					sum[j] = count;
-				}else{
-					sum[j] = count;
-				}
-			}
-			if(start == 0) {
-				System.out.println(sum[end]);
-			}else {
-				System.out.println(sum[end] - sum[start - 1]);
+			for (int j = 0; j < 26; j++) {
+				int beforeValue = alpha[i - 1][j];
+				alpha[i][j] = (j == searchChar ? beforeValue + 1 : beforeValue);
 			}
 		}
+
+		for (int i = 0; i < Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int findIndex = st.nextToken().charAt(0)-'a';
+			int start = Integer.parseInt(st.nextToken())+1;
+			int end = Integer.parseInt(st.nextToken())+1;
+
+			bw.write((alpha[end][findIndex]-alpha[start-1][findIndex])+"\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
 	}
 }
