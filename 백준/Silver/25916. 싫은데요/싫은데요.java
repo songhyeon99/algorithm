0ab19c25c421b68b686be673508242719fda76ae
Songhyeon Seo <songhyeon4643@gmail.com>
2024-09-03 20:23:04 +0900

    [Silver I] Title: 싫은데요, Time: 380 ms, Memory: 66648 KB -BaekjoonHub

---
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int left = 0;
+		int right = 0;
+		long count = 0;
+		long result = 0;
+		while (right < N) {
+			count += arr[right];
+			while(count > M){
+				count -= arr[left];
+				left++;
+			}
+			result = Math.max(result, count);
+			right++;
+		}
+		System.out.println(result);
+	}
+}
