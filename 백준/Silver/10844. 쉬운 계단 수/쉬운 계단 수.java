015c7b6a600dd57898d6f80d684c4555ab6f2da9
Songhyeon Seo <songhyeon4643@gmail.com>
2024-09-10 21:01:26 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 168 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -5,21 +5,22 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
-		long[][] dp = new long[101][10];
+		long[][] dp = new long[N+1][10];
 		long MOD = 1000000000L;
 
 		for (int i = 1; i < 10; i++) {
-			dp[1][i] = 1;
+			dp[1][i] = 1 % MOD;
 		}
-
 		for (int i = 2; i <= N; i++) {
 			dp[i][0] = dp[i - 1][1] % MOD;
-			for (int j = 1; j <= 8; j++)
-				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+			for (int j = 1; j <= 8; j++) {
+				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1] % MOD;
+			}
 			dp[i][9] = dp[i - 1][8] % MOD;
 		}
+
 		long result = 0;
-		for (int i = 0; i <= 9; i++) {
+		for (int i = 0; i < 10; i++) {
 			result = (result + dp[N][i]) % MOD;
 		}
 		System.out.println(result);
