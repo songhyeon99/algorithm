5c0671a08e94d3330e450dfdef38169aa98069e4
Songhyeon Seo <songhyeon4643@gmail.com>
2024-09-18 15:36:11 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -15,10 +15,11 @@ public class Main {
 		}
 
 		dp[1] = arr[1];
-		if (N >= 2)
+		if(N >= 2) {
 			dp[2] = arr[1] + arr[2];
-		for (int i = 3; i <= N; i++) {
-			dp[i] = Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
+			for (int i = 3; i <= N; i++) {
+				dp[i] = Math.max(dp[i-2], dp[i-3]+arr[i-1])+ arr[i];
+			}
 		}
 		System.out.println(dp[N]);
 	}
