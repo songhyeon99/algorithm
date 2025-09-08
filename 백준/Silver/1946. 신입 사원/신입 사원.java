09b754f198bd1ffaa04c7e6975b146da8ee4a8ae
Songhyeon Seo <songhyeon4643@gmail.com>
2025-09-08 23:58:59 +0900

    [Silver I] Title: 신입 사원, Time: 2840 ms, Memory: 318352 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,44 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
 
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-
-		for (int i = 0; i < T; i++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int count = 1;
-			int[][] arr = new int[N][2];
-			boolean[] visited = new boolean[N];
-
-			for (int j = 0; j < N; j++) {
-				st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<n; i++){
+			int num = Integer.parseInt(br.readLine());
+			int[][] arr = new int[num][2];
+			for(int j = 0; j<num; j++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
 				arr[j][0] = Integer.parseInt(st.nextToken());
 				arr[j][1] = Integer.parseInt(st.nextToken());
 			}
-
-			Arrays.sort(arr, (o1, o2) -> {
-				return o1[0] - o2[0];
-			});
-
-			int min = arr[0][1];
-			for (int j = 1; j < N; j++) {
-				if (min > arr[j][1]) {
-					count++;
-					min = arr[j][1];
-				}
-			}
-			sb.append(count).append("\n");
+			System.out.println(newEmployee(num, arr));
 		}
-		System.out.println(sb);
+	}
+
+	public static int newEmployee(int n, int[][] arr){
+		int count = 0;
+		Arrays.sort(arr, (o1, o2) -> {
+			return o1[0] - o2[0];
+		});
+		int min = arr[0][1];
+		for(int i = 1;i<n;i++){
+			if(min > arr[i][1]) {
+				count++;
+				min = arr[i][1];
+			}
+		}
+		return count+1;
 	}
 }
