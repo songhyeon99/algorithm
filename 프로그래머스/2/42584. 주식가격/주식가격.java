de347821fc5ecd98e7302ff65c35e441b6caafaa
Songhyeon Seo <songhyeon4643@gmail.com>
2026-03-15 15:44:40 +0900

    [level 2] Title: 주식가격, Time: 35.35 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,17 +1,24 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        for(int i = 0; i<prices.length; i++) {
-            int cur = prices[i];
-            answer[i] = 0;
-            for(int j = i+1; j<prices.length; j++) {
-                if(prices[j] >= cur) {
-                    answer[i]++;
-                }else{
-                    answer[i]++;
-                    break;
-                }
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for(int i = 0; i<prices.length; i++){
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                int currentIdx = stack.pop();
+                answer[currentIdx] = i - currentIdx;
             }
+                stack.push(i);
+            
+        }
+        
+        int stackSize = stack.size();
+        for(int i = 0; i<stackSize; i++) {
+                int currentIdx = stack.pop();
+                answer[currentIdx] = prices.length - 1 - currentIdx;
         }
         return answer;
     }
