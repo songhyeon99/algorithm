ce70274d5d31b51afd258440817ed968db9c0f4d
Songhyeon Seo <songhyeon4643@gmail.com>
2026-04-03 23:45:39 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,29 +1,29 @@
 import java.util.*;
+import java.io.*;
+
 class Solution {
     static boolean[] visited;
+    static int answer = 0;
+    
     public int solution(int n, int[][] computers) {
         visited = new boolean[n];
-        int count = 0;
-        for(int i = 0; i<n; i++){
-            if(!visited[i]){
-                bfs(i, n, computers);
-                count++;
+        
+       for(int i = 0; i < n ; i++) {
+            if(!visited[i]) {
+                answer++;
+                dfs(i, computers, n);
             }
         }
-        return count;
-    }
-    static void bfs(int current, int n, int[][] computers){
-        Queue<Integer> queue = new LinkedList<>();
-        visited[current] = true;
-        queue.add(current);
         
-        while(!queue.isEmpty()){
-            int num = queue.poll();
-            for(int i = 0; i<n; i++){
-                if(!visited[i] && num != i && computers[num][i] == 1){
-                    queue.add(i);
-                    visited[i] = true;
-                }
+        return answer;
+    }
+    
+    static void dfs(int x, int[][] computers, int n) {
+        visited[x] = true;
+        
+        for(int i = 0; i < n ; i++) {
+            if(computers[x][i] == 1 && !visited[i] && x != i) {
+                dfs(i, computers, n);
             }
         }
     }
