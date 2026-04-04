3903e1ebdbc276e35238b2f32035a72ad7769aa1
Songhyeon Seo <songhyeon4643@gmail.com>
2026-04-04 15:45:26 +0900

    [level 1] Title: 체육복, Time: 0.33 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 0.50 ms
+메모리: 90.3 MB, 시간: 0.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 15:25:01
+2026년 04월 04일 15:45:22
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,46 +1,53 @@
 import java.util.*;
+import java.io.*;
+
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int count = 0;
+        int answer = 0;
         
         List<Integer> lostList = new ArrayList<>();
         List<Integer> reserveList = new ArrayList<>();
+        boolean[] visited = new boolean[n+1];
         
-        for (int l : lost) {
-            lostList.add(l);
-        }
-        for (int r : reserve) {
-            reserveList.add(r);
+        for(int i = 0; i < lost.length; i++) {
+            lostList.add(lost[i]);
         }
         
-        List<Integer> intersection = new ArrayList<>(lostList);
-        intersection.retainAll(reserveList);
-        
-        lostList.removeAll(intersection);
-        reserveList.removeAll(intersection);
-        
-        boolean[] isLost = new boolean[n+2];
-        Collections.sort(lostList);
-        Collections.sort(reserveList);
-        
-        for(int i = 0; i < lostList.size(); i++){
-            isLost[lostList.get(i)] = true;
+        for(int i = 0; i < reserve.length; i++) {
+            reserveList.add(reserve[i]);
         }
         
-        
-        for(int i = 0; i<reserveList.size(); i++){
-            if(isLost[reserveList.get(i)]){
-                isLost[reserveList.get(i)] = false;
-            }else if(isLost[reserveList.get(i)-1]){
-                isLost[reserveList.get(i)-1] = false;
-            }else if(isLost[reserveList.get(i)+1]){
-                isLost[reserveList.get(i)+1] = false;
+        for(int i = 0; i< lost.length; i++) {
+            if(reserveList.contains(lost[i])) {
+                reserveList.remove(Integer.valueOf(lost[i]));
+            }
+        }
+        for(int i = 0; i< reserve.length; i++) {
+            if(lostList.contains(reserve[i])) {
+                lostList.remove(Integer.valueOf(reserve[i]));
             }
         }
         
-        for(int i = 1; i<=n; i++){
-            if(!isLost[i]) count++;
+        Collections.sort(lostList);
+        Collections.sort(reserveList);
+        
+        answer = reserveList.size();
+        
+        for(int i = 1; i<=n; i++) {
+            if(reserveList.contains(i)) {
+                if(lostList.contains(i-1) && !visited[i-1]) {
+                    reserveList.remove(Integer.valueOf(i));
+                    visited[i-1] = true;
+                    answer++;
+                }else if(lostList.contains(i+1) && !visited[i+1]) {
+                    reserveList.remove(Integer.valueOf(i));
+                    visited[i+1] = true;
+                    answer++;
+                }
+            }else if(!reserveList.contains(i) && !lostList.contains(i)) {
+                answer++;
+            }
         }
-        return count;
+        return answer;
     }
 }
