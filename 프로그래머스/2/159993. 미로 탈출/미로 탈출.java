578f1d56ee55eea387e3411944f651fb61deaff6
Songhyeon Seo <songhyeon4643@gmail.com>
2026-04-05 01:16:08 +0900

    [level 2] Title: 미로 탈출, Time: 9.11 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,61 +1,63 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
-    static char[][] map;
-    static int[][] count;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
+    static char[][] charMaps;
+    
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    
+    
     public int solution(String[] maps) {
-        map = new char[maps.length][maps[0].length()];
-        count = new int[maps.length][maps[0].length()];
+        charMaps = new char[maps.length][maps[0].length()];
         
-        int startXForS = 0; int startYForS = 0;
-        int startXForL = 0; int startYForL = 0;
+        int startRow = 0; int startCol = 0;
+        int lebberRow = 0; int lebberCol = 0;
         
-        for(int i = 0; i<maps.length; i++){
-            for(int j = 0; j<maps[0].length(); j++){
-                map[i][j] = maps[i].charAt(j);
-                if(map[i][j] == 'S'){
-                    startXForS = i;
-                    startYForS = j;
+        for(int i = 0; i<maps.length; i++) {
+            String row = maps[i];
+            for(int j = 0; j<maps[i].length(); j++ ) {
+                charMaps[i][j] = row.charAt(j);
+                if(row.charAt(j) == 'S') {
+                    startRow = i;
+                    startCol = j;
                 }
-                if(map[i][j] == 'L') {
-                    startXForL = i;
-                    startYForL = j;
+                if(row.charAt(j) == 'L') {
+                    lebberRow = i;
+                    lebberCol = j;
                 }
             }
         }
         
-        int toLever = bfs(startXForS, startYForS, 'L');
-        if(toLever == -1) return -1; 
+        int toLebber = bfs(startRow, startCol, 'L', charMaps);
+        if(toLebber == -1) return -1;
+        int toEnd = bfs(lebberRow, lebberCol, 'E', charMaps);
+        if(toEnd == -1) return -1;
         
-        int toExit = bfs(startXForL, startYForL, 'E');
-        if(toExit == -1) return -1;
-        
-        return toLever + toExit;
+        return toLebber + toEnd;
     }
     
-    public static int bfs(int x, int y, int destination){
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        Queue<Position> queue = new LinkedList<>();
-        queue.add(new Position(x,y, 0));
+    int bfs(int row, int col, char to, char[][] maps) {
+        Queue<Position> q = new LinkedList<>();
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
         
-        while(!queue.isEmpty()) {
-            Position position = queue.poll();
-            // if (map[position.x][position.y] == destination) return position.count;
-            for(int i = 0; i<4; i++){
-                int nx = dx[i] + position.x;
-                int ny = dy[i] + position.y;
-
-                if(nx >= 0 && nx <map.length && ny >= 0 && ny < map[0].length && 
-                   !visited[nx][ny] && map[nx][ny] != 'X'
-                ) {
-                    if(map[nx][ny] == destination) {
-                            return position.count + 1;
-                    }else {
-                        visited[nx][ny] = true;
-                        queue.add(new Position(nx, ny, position.count+1));
+        q.add(new Position(row, col, 0));
+        visited[row][col] = true;
+        
+        while(!q.isEmpty()) {
+            Position current = q.poll();
+            for(int i = 0 ; i<4; i++) {
+                int newRow = dx[i] + current.row;
+                int newCol = dy[i] + current.col;
+                
+                
+                if(newRow >= 0 && newCol >= 0 && newRow < maps.length && newCol < maps[0].length) {
+                    if(!visited[newRow][newCol] && maps[newRow][newCol] != 'X'){
+                        q.add(new Position(newRow, newCol, current.count+1)); 
+                        visited[newRow][newCol] = true;
+                    }
+                    if(maps[newRow][newCol] == to) {
+                        return current.count+1;
                     }
                 }
             }
@@ -63,14 +65,12 @@ class Solution {
         return -1;
     }
     
-   static class Position {
-        int x;
-        int y;
-        int count;
+    class Position {
+        int row, col, count;
         
-        public Position(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
+        Position(int row, int col, int count) {
+            this.row = row;
+            this.col = col;
             this.count = count;
         }
     }
